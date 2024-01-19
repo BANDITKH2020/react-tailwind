31acf4f3ce5c943b38c75e9471032399b841d9ed
Bandit Khawyen <banditkh2020@gmail.com>
2024-01-19 14:37:48 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <div>
+                    <a href="/">Logo</a>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
+
